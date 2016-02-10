1438ce84a8ee5cb209217195b9115721d5096f9e
Jirat Arinrith <jepezi@gmail.com>
2016-02-10 11:17:56 +0700

    render client with match (or normal way) / Perf

--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill';
+
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, match } from 'react-router';
 import { Provider } from 'react-redux';
 
 import configureStore from './redux/configureStore';
@@ -11,9 +12,36 @@ const store = configureStore(window.__JSDATA__);
 store.dispatch({type: 'greet', name: 'jip'});
 store.dispatch({type: 'greet', name: 'hazel'});
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router routes={routes} history={browserHistory} />
-  </Provider>,
-  document.getElementById('app')
-)
+
+
+
+// Render
+
+
+
+// (1) normal render
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router routes={routes} history={browserHistory} />
+//   </Provider>,
+//   document.getElementById('app')
+// )
+
+
+
+// (2) use match
+
+match({ routes, location }, () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router routes={routes} history={browserHistory} />
+    </Provider>,
+    document.getElementById('app')
+  )
+})
+
+// Perf
+if (process.env.NODE_ENV !== 'production') {
+  window.Perf = require('react-addons-perf');
+}
